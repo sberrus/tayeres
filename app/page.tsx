291a0235352fe45ec4c8032942bf68fb8235a643
MrUnknown-S <xsilent1464@gmail.com>
2023-07-16 13:54:45 +0200

    build: :art: Add better eslint config and add prettier for tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react'
 
 const Page = () => {
   return (
     <div>
-      <h1 style={{ textAlign: "center", fontSize: "3em" }}>
+      <h1 style={{ textAlign: 'center', fontSize: '3em' }}>
         100 Billion Dolar To-Do app
       </h1>
       <video
@@ -12,10 +12,10 @@ const Page = () => {
         autoPlay
         loop
         controls
-        style={{ height: "75vh", margin: "auto" }}
+        style={{ height: '75vh', margin: 'auto' }}
       ></video>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
